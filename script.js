917134c2ebed7a81d0c1414f53235666a37fa038
Tomasz Brylski <brylskitomasz@gmail.com>
2024-06-09 17:54:50 +0200

    Fixed typos in the code in the line containing: - keys.length-1, - keyElement

--- script.js
+++ script.js
@@ -11,36 +11,36 @@ function getRandomNumber(min, max) {
 }
 
 function getRandomKey() {
-    return keys[getRandomNumber(0, keys.lenght-1)]
+  return keys[getRandomNumber(0, keys.length-1)]
 }
 
 function targetRandomKey() {
-    const key = document.getElementById(getRandomKey());
-    key.classList.add("selected");
-    let start = Date.now()
+  const key = document.getElementById(getRandomKey());
+  key.classList.add("selected");
+  let start = Date.now()
 }
 
 function getTimestamp() {
-    return Math.floor(Date.now() / 1000)
+  return Math.floor(Date.now() / 1000)
 }
 
 document.addEventListener("keyup", event => {
-    const keyPressed = String.fromCharCode(event.keyCode);
-    const KeyElement = document.getElementById(keyPressed);
-    const highlightedKey = document.querySelector(".selected");
-
-    KeyElement.classList.add("hit")
-    KeyElement.addEventListener('animationend', () => {
-        KeyElement.classList.remove("hit")
-    })
-
-    if (keyPressed === highlightedKey.innerHTML) {
-        timestamps.unshift(getTimestamp());
-        const elapsedTime = timestamps[0] - timestamps[1];
-        console.log(`Character per minute ${60/elapsedTime}`)
-        highlightedKey.classList.remove("selected");
-        targetRandomKey();
-    }
+  const keyPressed = String.fromCharCode(event.keyCode);
+  const keyElement = document.getElementById(keyPressed);
+  const highlightedKey = document.querySelector(".selected");
+  
+  keyElement.classList.add("hit")
+  keyElement.addEventListener('animationend', () => {
+    keyElement.classList.remove("hit")
+  })
+  
+  if (keyPressed === highlightedKey.innerHTML) {
+    timestamps.unshift(getTimestamp());
+    const elapsedTime = timestamps[0] - timestamps[1];
+    console.log(`Character per minute ${60/elapsedTime}`)
+    highlightedKey.classList.remove("selected");
+    targetRandomKey();
+  } 
 })
 
 targetRandomKey();
